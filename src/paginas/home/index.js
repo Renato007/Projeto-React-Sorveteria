2d9bb8de74a3c5e7bed3ea83d659c6ede2313a8b
Renato Soares <renatosilsoa@gmail.com>
2024-01-18 15:08:11 -0300

    Estilização e responsividade do componente Home

--- src/paginas/home/index.js
+++ src/paginas/home/index.js
@@ -1,53 +1,56 @@
-import React from "react";
-import Topo from "../../compomentes/Topo";
-import Rodape from "../../compomentes/Rodape";
+import Topo from "../../compomentes/Topo"
+import Rodape from "../../compomentes/Rodape"
 
 import "./style.css"
 
 export default function Home() {
-  return (
+  return(
     <div>
+
       <Topo />
+
       <main>
-        <section>
-          <div>
-            <h1>Sorvete Artesanal</h1>
+        <section className="secao-banner">
+          <div className="texto-banner">
+            <h1>SORVETE ARTESANAL</h1>
           </div>
         </section>
 
-        <section>
+        <section className="secao-nossos-sabores">
           <img src="assets/img/banner-sabores.jpg" alt="sorvetes diversos" />
-          <h2>Novos e deliciosos!</h2>
-          <p>
-            Sorvete bom é aquele feito com os melhores ingredientes! Aqui na
-            gelateria todos os nossos produtos são naturais, à base de frutas e
-            sem nenhum conservante! Também temos opções sem lactose e sem
-            açúcar. Venha conhecer e perceber que tem como o sorvete ser
-            delicioso e saudável ao mesmo tempo!
-          </p>
-        </section>
-
-        <section>
-          <div>
-            <h2>Nossoss Eventos</h2>
-            <span>Delicias com sorvete</span>
+          <div className="texto-secao-nossos-sabores">
+            <h2>NOSSOS SABORES</h2>
+            <span>Novos e deliciosos!</span>
             <p>
-              Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui
-              prontinhos para te atender e oferecer os melhores eventos com os
-              melhores sorvete da sua vida! Venha nos conhecer e passar um tempo
-              aqui com a gente.
+              Sorvete bom é aquele feito com os melhores ingredientes! Aqui na
+              gelateria todos os nossos produtos são naturais, à base de
+              frutas e sem nenhum conservante! Também temos opções sem lactose
+              e sem açúcar. Venha conhecer e perceber que tem como o sorvete
+              ser delicioso e saudável ao mesmo tempo!
             </p>
           </div>
-          <img src="assets/img/eventos-image.jpg" alt="mesa de festa"/>
         </section>
 
-        <section>
-          <img src="assets/img/sobre-image.jpg" alt="pessoas comendo sorvete"/>
-          <div>
+        <section className="secao-nossos-eventos">
+          <div className="texto-secao-nossos-eventos">
+              <h2>NOSSOS EVENTOS</h2>
+              <span>Delicias com sorvete!</span>
+              <p>
+                Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui
+                prontinhos para te atender e oferecer os melhores eventos com os melhores
+                sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.
+              </p>
+          </div>
+          <img src="assets/img/eventos-image.jpg" alt="mesa de festa" />
+        </section>
+
+        <section className="secao-sobre-nos">
+          <img src="assets/img/sobre-image.jpg" alt="pessoas comendo sorvete" />
+          <div className="texto-secao-sobre-nos">
             <h2>SOBRE NÓS</h2>
-            <span> Alegria em cada casquinha!</span>
+            <span>Alegria em cada casquinha!</span>
             <p>
-            Venha tomar o melhor sorvete da região aqui com a gente! Nós
+              Venha tomar o melhor sorvete da região aqui com a gente! Nós
               estamos há anos no mercado produzindo o que tem de melhor para o
               nosso cliente e você não pode ficar fora dessa. Venha nos fazer
               uma visita e aproveite o melhor atendimento e o melhor sorvete
@@ -56,7 +59,9 @@ export default function Home() {
           </div>
         </section>
       </main>
+
       <Rodape />
+
     </div>
-  );
+  )
 }
